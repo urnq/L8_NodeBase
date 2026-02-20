a839d0b2d913a53d18f2340cf762d88bed1a8876
Nikita <Voloskonikita0@gmail.com>
2026-02-20 22:47:28 +0300

    add .env...

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-require('dotenv').config(); 
+// require('dotenv').config(); 
 
-console.log('Информация из .env файла:');
-console.log('Имя:', process.env.MY_NAME);
-console.log('Фамилия:', process.env.MY_SURNAME);
-console.log('Группа:', process.env.MY_GROUP);
-console.log('Номер по списку:', process.env.MY_LIST_NUMBER);
-console.log('Режим доступа:', process.env.MODE);
+// console.log('Информация из .env файла:');
+// console.log('Имя:', process.env.MY_NAME);
+// console.log('Фамилия:', process.env.MY_SURNAME);
+// console.log('Группа:', process.env.MY_GROUP);
+// console.log('Номер по списку:', process.env.MY_LIST_NUMBER);
+// console.log('Режим доступа:', process.env.MODE);
+
+require('dotenv').config();
+const osFunctions = require('./os'); 
+
+osFunctions.getBasicOSInfo();
+osFunctions.isFreeMemoryMoreThan4GB();
+osFunctions.getOSInfoByMode(process.env.MODE);
